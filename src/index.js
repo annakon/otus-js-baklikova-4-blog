5bd1233c6204a4b91d8924ecfed7396465c31b1d
denis <denis.baklykov@boxberry.ru>
2022-10-29 17:29:16 +0300

    пробрасывать объект

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import "../scss/styles.scss";
 import * as Sim from "./sim-slider";
 
 document.addEventListener("DOMContentLoaded", () => {
-  new Sim("slider1");
+  new Sim("slider1",{
+    loop: true, // Бесконечное зацикливание слайдера
+    auto: true, // Автоматическое пролистывание
+    interval: 5000, // Интервал между пролистыванием элементов (мс)
+    arrows: true, // Пролистывание стрелками
+    dots: true, // Индикаторные точки
+  });
 });
